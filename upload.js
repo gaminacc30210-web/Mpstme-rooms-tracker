7d0b1da2db134ccd443a1d3567fcb0d2a1ea33bc
gaminacc30210-web <gaminacc30210@gmail.com>
2026-09-16 13:01:25 +0530

    upload.js

--- upload.js
+++ upload.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const multer = require("multer");
-const { extractTimetableFields } = require("../lib/gemini");
+const { extractTimetableFields } = require("./gemini");
 
 const router = express.Router();
 
